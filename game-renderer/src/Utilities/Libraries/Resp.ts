8b509afa38e1bf6f2b6764b5f8116119c06bce44
Nexus <108929448+j5vz@users.noreply.github.com>
2026-03-13 16:08:34 +0300

    Delete game-renderer directory

--- game-renderer/src/Utilities/Libraries/Resp.ts
+++
@@ -1,9 +0,0 @@
-import {Response} from "ultimate-express";
-
-export default (res: Response, status?: number | null, message?: string | null, success?: boolean | null, additionalData?: any | null) => {
-    return res.status(status || 200).send({
-        success: success === null || success === undefined ? true : success,
-        message: message || "Success",
-        ...additionalData
-    });
-};
